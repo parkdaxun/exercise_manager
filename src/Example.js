34fc17af9875dc6c0c46d18f667f8399cd38a010
parkdaxun <parkde0207@gmail.com>
2022-06-25 00:12:39 +0900

    버튼 수정2

--- src/Example.js
+++ src/Example.js
@@ -7,14 +7,10 @@ export default class Example extends React.Component {
         super(props);
     }
 
-    const start_recode = () => {
-
-    }
-
     render() {
         return (
             <div>
-                <button className="recode" onClick={start_recode}>운동 기록하기</button>
+                <button className="recode">운동 기록하기</button>
                 <button className="look">운동 목록보기</button>
                 <button className="check">기록 확인하기</button>
             </div>
