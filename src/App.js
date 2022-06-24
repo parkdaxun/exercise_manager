d117382b0aef8e85c3d82ccd712cb6a22b53c853
parkdaxun <parkde0207@gmail.com>
2022-06-24 14:59:21 +0900

    약간 건들기? (react 어려움 이해가 안됨 하 ㅜ)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  return (
+      <div className="nav">
+        <h2>운동 매니저</h2>
+          <h1>💪🏻홈트레이닝</h1>
+          <p>당신의 건강을 책임져 드립니다.</p>
+      </div>
+  );
 }
 
 export default App;
