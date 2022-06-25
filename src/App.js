05afec608c6e010792315cd144d89ea3781f8b7d
parkdaxun <parkde0207@gmail.com>
2022-06-25 22:28:40 +0900

    운동 목록 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Example from "./Example";
+import Example from './Example.js';
 
 function App() {
   return (
       <div className="nav">
         <h2>운동 매니저</h2>
+          <hr></hr>
           <h1>💪🏻홈트레이닝</h1>
           <p>당신의 건강을 책임져 드립니다.</p>
           <Example/>
--- src/home.js
+++ src/home.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import './App.css';
+import './home.css';
 
 export default class home extends React.Component {
 
