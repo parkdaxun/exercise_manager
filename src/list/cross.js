f05ea25e0f7c60d641b8d469ccf6c8128ceeada9
parkdaxun <parkde0207@gmail.com>
2022-06-27 02:14:22 +0900

    유튜브로 이동, 영상 시청 넣기

--- src/list/cross.js
+++ src/list/cross.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './cross.css';
+import {Link} from "react-router-dom";
 
 export default class cross extends React.Component {
 
@@ -11,7 +12,9 @@ export default class cross extends React.Component {
         return (
             <div className="info">
                 <img className="cross_run" src={require("./images/cross_run.png")}/>
-                <text1>시작하기</text1>
+                <div className="cross_watch">
+                    <a href="https://youtu.be/3_4GVbcno0A">영상 시청하기</a>
+                </div>
             </div>
         );
     };
